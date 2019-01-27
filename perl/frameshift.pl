35c4306cd62687776368d0f2f3e5a217c53d62f1
cseppey <christophe.seppey@uit.no>
2019-01-27 09:38:09 +0100

    adjust the aminoacid code + adjusting the modulo (need to add argument to do it from the command line)

--- perl/frameshift.pl
+++ perl/frameshift.pl
@@ -4,9 +4,6 @@ use warnings;
 use strict;
 
 
-
-
-
 open (my $IN_FH, "<$ARGV[0]");
 
 my $prefix = $ARGV[1];
@@ -18,12 +15,12 @@ open (my $OUT_NUCL_FH, ">$out_nucl");
 
 #---
 
-my %aacode = (
+my %aacode = ( # bacterial archaea code
   TTT => "F", TTC => "F", TTA => "L", TTG => "L",
   TCT => "S", TCC => "S", TCA => "S", TCG => "S",
   TAT => "Y", TAC => "Y", TAA => "_", TAG => "_",
   TGT => "C", TGC => "C", TGA => "_", TGG => "W",
-  CTT => "L", CTC => "L", CTA => "L", CTG => "L",
+  CTT => "L", CTC => "L", CTA => "L", CTG => "S",
   CCT => "P", CCC => "P", CCA => "P", CCG => "P",
   CAT => "H", CAC => "H", CAA => "Q", CAG => "Q",
   CGT => "R", CGC => "R", CGA => "R", CGG => "R",
@@ -45,33 +42,37 @@ while (my $ligne = <$IN_FH>) {
     $titre = $ligne;
   }
   else {
-    my $l = length $ligne;
-    if (($ligne !~ /^((.{3})*?)(TGA|TAG|TAA).*/) & ($l % 3 == 1)) {
+    $ligne = substr $ligne, $ARGV[2];
+    $ligne = substr $ligne, 0, -($ARGV[3]+1);
 
-      print $OUT_NUCL_FH "$titre$ligne";
+    my $l = length $ligne;
+    
+    if (($ligne !~ /^((.{3})*?)(TGA|TAG|TAA).*/) & ($l % 3 == 0)) {
+
+      print $OUT_NUCL_FH $titre;
+      print $OUT_NUCL_FH "$ligne\n";
 
       print $OUT_PROT_FH $titre;
-      
+
       while ($l >= 3) {
         my $codon = substr $ligne, 0, 3;
 
-	if ($codon =~ 'N') {
-	  print $OUT_PROT_FH '-';
-	}
-	else {
-	  print $OUT_PROT_FH "$aacode{$codon}";
-	}
+        if ($codon =~ 'N') {
+          print $OUT_PROT_FH '-';
+        }
+        else {
+          print $OUT_PROT_FH "$aacode{$codon}";
+        }
 
         $ligne = substr $ligne, 3;
         $l = length $ligne;
       }
       print $OUT_PROT_FH "\n";
     }
-
   }
 
 }
 
 close $IN_FH;
-close $OUT_PROT_FH;
 close $OUT_NUCL_FH;
+close $OUT_PROT_FH;
